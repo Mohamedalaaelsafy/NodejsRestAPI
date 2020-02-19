25bccdd5000bd9851882ff32c2e2bd12ade2e89b
Mohamedalaaelsafy <56837043+Mohamedalaaelsafy@users.noreply.github.com>
2020-02-19 05:34:00 +0200

    better

--- api/controllers/doctorControl.js
+++ api/controllers/doctorControl.js
@@ -113,7 +113,7 @@ exports.signin =  (req , res ,  next)=>{
                 return res.status(200).json({
                     message: 'Auth successful',
                     token: token,
-                    doctor: doctor.toLocaleString
+                    doctor: Object[doctor]
                 })
             }
               res.status(401).json({
