fcf6d91ca86b073a926fcb6394c906034eaacac4
Mohamedalaaelsafy <56837043+Mohamedalaaelsafy@users.noreply.github.com>
2020-02-19 05:56:08 +0200

    better

--- api/controllers/doctorControl.js
+++ api/controllers/doctorControl.js
@@ -113,7 +113,7 @@ exports.signin =  (req , res ,  next)=>{
                 return res.status(200).json({
                     message: 'Auth successful',
                     token: token,
-                    doctor: Object.assign({},doctor)
+                    doctor: Object.assign(doctor.map(k=>({[k]:req.body})))
                 })
             }
               res.status(401).json({
